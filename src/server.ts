5e263561a73fc1cef8809df1e74afd4547af5f47
shayan.heidary <shayan.heidary@knime.com>
2024-07-25 19:47:48 +0200

    completed config

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,6 @@ import router from "./routers";
 import cors from "cors";
 import { createUser, loginUser } from "./handlers/user";
 import { protect } from "./modules/auth";
-import OpenAI from 'openai';
 
 /**
  * App
@@ -43,53 +42,7 @@ app.use("/api", protect, router);
 app.post("/signin", createUser);
 app.post("/login", loginUser);
 
-app.get("/chat", async(req,res)=>{
 
-  // const openai = new OpenAI({
-  //   apiKey: "sk-proj-b3mxwV5L1WkhaYjsApF1T3BlbkFJzhZjkbw9lBnZkst9SyTp", // This is the default and can be omitted
-  // });
-  
-  // async function main() {
-  //   const chatCompletion = await openai.chat.completions.create({
-  //     messages: [{ role: 'user', content: 'Say this is a test' }],
-  //     model: 'gpt-3.5-turbo',
-  //   });
-  //   console.log("res",chatCompletion)
-  // }
-  // main();
-
-  const url = 'https://chatgpt-42.p.rapidapi.com/conversationgpt4-2';
-const options = {
-	method: 'POST',
-	headers: {
-		'x-rapidapi-key': '05800b0e55msh61a4e11ced57cfbp1cf8f0jsn2c26d2886151',
-		'x-rapidapi-host': 'chatgpt-42.p.rapidapi.com',
-		'Content-Type': 'application/json'
-	},
-	body: JSON.stringify({
-		messages: [
-			{
-				role: 'user',
-				content: 'hello'
-			}
-		],
-		system_prompt: '',
-		temperature: 0.9,
-		top_k: 5,
-		top_p: 0.9,
-		max_tokens: 256,
-		web_access: false
-	})
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result , "res");
-} catch (error) {
-	console.error(error);
-}
-})
 
 app.use((err, req, res, next) => {
   console.log(`something went wrong : ${err}`);
